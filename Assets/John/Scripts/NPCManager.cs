d4b96deaf46471283e5f1d27165d22679fda5231
John <1019178@student.sae.edu.au>
2021-10-21 20:55:50 +1100

    Spawn System Base

---
+++ Assets/John/Scripts/NPCManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour
+{
+    public static List<Transform> availableSpawns = new List<Transform>();
+
+    public List<NPC> remainingEnemies;
+
+    public GameObject NPC;
+
+    public GameObject Boss;
+
+    public int nextSpawn;
+
+    public int waveCount;
+    public float waveDelay;
+    
+    
+
+    public static void AddSpawnPoint(Transform transform)
+    {
+        availableSpawns.Add(transform);
+    }
+    public static void RemoveSpawnPoint(Transform transform) => availableSpawns.Remove(transform);
+
+    void SpawnNPC()
+    {
+        //do the spawn thing ya feel...
+    }
+}
---
+++ Assets/John/Scripts/NPC_Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NPC_Spawner : MonoBehaviour
+{
+    public void Awake() => NPCManager.AddSpawnPoint(transform);
+    
+    private void OnDestroy() => NPCManager.RemoveSpawnPoint(transform);
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 0.25f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position,transform.position + transform.forward * 2);
+    }
+}

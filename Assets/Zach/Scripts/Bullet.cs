225474bd7ebb904c50eedcb4fcf189a184da81b9
Venomlord123 <1019559@student.sae.edu.au>
2021-10-25 18:03:26 +1100

    Gun and Bullet Test

---
+++ Assets/Zach/Scripts/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody rb;
+    //todo set this properly when doing polling
+    //Set this before playing in scene
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+}
---
+++ Assets/Zach/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(bullet, transform.position, new Quaternion());
+        }
+    }
+}

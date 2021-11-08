cc12388202f2ae7b2984e0c74c0dd9b733b265bb
John <1019178@student.sae.edu.au>
2021-11-08 18:17:54 +1100

    Game Manager

---
+++ Assets/Team Members/John/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class GameManager : MonoBehaviour
+{
+    public NPCManager NpcManager;
+    public float startDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FirstWave());
+    }
+
+    public IEnumerator FirstWave()
+    {
+        yield return new WaitForSeconds(startDelay);
+        StartCoroutine(NpcManager.Spawn());
+
+    }
+
+}

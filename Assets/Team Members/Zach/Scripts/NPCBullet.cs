2c4e16c453dfe5d5f2535285f76f07ecafdbd36b
Venomlord123 <1019559@student.sae.edu.au>
2021-11-16 00:29:38 +1100

    NPC Gun and Bullet Complete - Prefab updated and working

---
+++ Assets/Team Members/Zach/Scripts/NPCBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Liminal.SDK.Extensions;
+using Liminal.SDK.VR.Avatars;
+using UnityEngine;
+
+public class NPCBullet : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    //todo set this properly when doing polling
+    //Set this before playing in scene
+    public float speed;
+    public float time;
+    public float lifeTime;
+
+    public int damage;
+
+
+    private void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time >= lifeTime)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void OnEnable()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+
+    public void OnDisable()
+    {
+        time = 0;
+        rb.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.HasComponent<VRAvatar>())
+        {
+            other.GetComponent<Health>().Damage(1);
+        }
+    }
+}
---
+++ Assets/Team Members/Zach/Scripts/NPCGun.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Liminal.SDK.VR.Avatars;
+using UnityEngine;
+
+public class NPCGun : MonoBehaviour
+{
+    //Object Pooling Variables
+    public static NPCGun SharedInstance;
+    public List<GameObject> lasers;
+    public GameObject laser;
+    public int amountToPool;
+    //Variables
+    public float timeTillShoot;
+    public float shootTime;
+    public bool canShoot;
+    public Transform playerLocation;
+    public AudioSource gunSFX;
+    
+    
+    public void Awake()
+    {
+        SharedInstance = this;
+        playerLocation = FindObjectOfType<VRAvatarHead>().GetComponent<Transform>();
+        canShoot = true;
+    }
+    
+    public void Start()
+    {
+        lasers = new List<GameObject>();
+        GameObject tempBullet;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tempBullet = Instantiate(laser);
+            tempBullet.SetActive(false);
+            lasers.Add(tempBullet);
+        }
+
+        timeTillShoot = shootTime;
+    }
+
+
+    public void Update()
+    {
+        timeTillShoot -= Time.deltaTime;
+
+        gameObject.transform.LookAt(playerLocation);
+        
+        if (timeTillShoot <= 0 && canShoot)
+        {
+            canShoot = false;
+            GameObject laser = SharedInstance.GetPooledObject();
+            if (laser != null)
+            {
+                laser.transform.position = transform.position;
+                laser.transform.rotation = transform.rotation;
+                laser.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+                laser.SetActive(true);
+                gunSFX.Play();
+            }
+
+            timeTillShoot = shootTime;
+            canShoot = true;
+        }
+        
+    }
+
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < amountToPool; i++)
+        {
+            if (!lasers[i].activeInHierarchy)
+            {
+                return lasers[i];
+            }
+        }
+
+        return null;
+    }
+}

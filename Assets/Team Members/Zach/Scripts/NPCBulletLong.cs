5c00e374cc179980df2193c2f24e45693a1206d7
Venomlord123 <1019559@student.sae.edu.au>
2021-11-22 17:01:10 +1100

    Long Laser Works

---
+++ Assets/Team Members/Zach/Scripts/NPCBulletLong.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBulletLong : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    //todo set this properly when doing polling
+    //Set this before playing in scene
+    public float speed;
+    public float time;
+    public float lifeTime;
+    public Vector3 originalScale;
+    public Vector3 maxScale;
+
+    public int damage;
+    public Vector3 tranfromScale;
+
+
+    public void Awake()
+    {
+        originalScale = transform.localScale;
+        tranfromScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        time = time + Time.deltaTime;
+        if (time >= lifeTime)
+        {
+            gameObject.SetActive(false);
+        }
+        
+        if (tranfromScale.x <= maxScale.x)
+        {
+            transform.localScale += new Vector3(1f,0,0);
+            tranfromScale = transform.localScale;
+        }
+    }
+    
+    //TODO Make the bullet scale over time instead of instantly using maybe a IEnumerator
+    
+    public void OnEnable()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+
+    public void OnDisable()
+    {
+        time = 0;
+        rb.velocity = Vector3.zero;
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = originalScale;
+        tranfromScale = originalScale;
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.HasComponent<VRAvatar>())
+        {
+            other.GetComponent<Health>().Damage(1);
+        }
+    }*/
+}
--- Assets/Team Members/Zach/Scripts/NPCGun.cs
+++ Assets/Team Members/Zach/Scripts/NPCGun.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Liminal.SDK.VR.Avatars;
 using UnityEngine;
 
@@ -8,33 +6,47 @@ public class NPCGun : MonoBehaviour
 {
     //Object Pooling Variables
     public static NPCGun SharedInstance;
-    public List<GameObject> lasers;
-    public GameObject laser;
+    public List<GameObject> straightLasers;
+    public List<GameObject> longLasers;
+    public GameObject straightLaser;
+    public GameObject longLaser;
     public int amountToPool;
+
+
     //Variables
     public float timeTillShoot;
     public float shootTime;
+    public float chanceToShoot;
     public bool canShoot;
     public Transform playerLocation;
     public AudioSource gunSFX;
-    
-    
+
+
     public void Awake()
     {
         SharedInstance = this;
         playerLocation = FindObjectOfType<VRAvatarHead>().GetComponent<Transform>();
         canShoot = true;
     }
-    
+
     public void Start()
     {
-        lasers = new List<GameObject>();
-        GameObject tempBullet;
+        straightLasers = new List<GameObject>();
+        GameObject tempStraightLaser;
         for (int i = 0; i < amountToPool; i++)
         {
-            tempBullet = Instantiate(laser);
-            tempBullet.SetActive(false);
-            lasers.Add(tempBullet);
+            tempStraightLaser = Instantiate(straightLaser);
+            tempStraightLaser.SetActive(false);
+            straightLasers.Add(tempStraightLaser);
+        }
+
+        longLasers = new List<GameObject>();
+        GameObject tempLongLaser;
+        for (int i = 0; i < amountToPool; i++)
+        {
+            tempLongLaser = Instantiate(longLaser);
+            tempLongLaser.SetActive(false);
+            longLasers.Add(tempLongLaser);
         }
 
         timeTillShoot = shootTime;
@@ -46,7 +58,7 @@ public class NPCGun : MonoBehaviour
         timeTillShoot -= Time.deltaTime;
 
         gameObject.transform.LookAt(playerLocation);
-        
+
         if (timeTillShoot <= 0 && canShoot)
         {
             canShoot = false;
@@ -54,16 +66,25 @@ public class NPCGun : MonoBehaviour
             if (laser != null)
             {
                 laser.transform.position = transform.position;
-                laser.transform.rotation = transform.rotation;
-                laser.GetComponent<Rigidbody>().AddForce(Vector3.forward);
-                laser.SetActive(true);
-                gunSFX.Play();
+                if (longLasers.Contains(laser))
+                {
+                    laser.transform.rotation = transform.rotation;
+                    laser.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+                    laser.SetActive(true);
+                    gunSFX.Play();
+                }
+                else
+                {
+                    laser.transform.rotation = transform.rotation;
+                    laser.GetComponent<Rigidbody>().AddForce(Vector3.forward);
+                    laser.SetActive(true);
+                    gunSFX.Play();
+                }
             }
 
             timeTillShoot = shootTime;
             canShoot = true;
         }
-        
     }
 
 
@@ -71,9 +92,20 @@ public class NPCGun : MonoBehaviour
     {
         for (int i = 0; i < amountToPool; i++)
         {
-            if (!lasers[i].activeInHierarchy)
+            float chance = Random.Range(0f, 1f);
+            if (chance >= chanceToShoot)
             {
-                return lasers[i];
+                if (!straightLasers[i].activeInHierarchy)
+                {
+                    return straightLasers[i];
+                }
+            }
+            else
+            {
+                if (!longLasers[i].activeInHierarchy)
+                {
+                    return longLasers[i];
+                }
             }
         }
 

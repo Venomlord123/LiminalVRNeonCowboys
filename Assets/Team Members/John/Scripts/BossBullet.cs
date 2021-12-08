8b894d44fddf24aa0a96b42d70128837c6938f66
Venomlord123 <1019559@student.sae.edu.au>
2021-12-08 16:52:32 +1100

    Boss Bullet Damages

--- Assets/Team Members/John/Scripts/BossBullet.cs
+++ Assets/Team Members/John/Scripts/BossBullet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Liminal.SDK.VR.Avatars;
 using UnityEngine;
 
 public class BossBullet : MonoBehaviour
@@ -11,5 +12,14 @@ public class BossBullet : MonoBehaviour
     private void Update()
     {
         transform.Rotate(_rotation * _speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponentInParent<VRAvatar>().GetComponentInChildren<DamageOverlay>())
+        {
+            gameObject.SetActive(false);
+            other.gameObject.GetComponentInParent<VRAvatar>().GetComponentInChildren<DamageOverlay>().Shot();
+        }
     }
 }

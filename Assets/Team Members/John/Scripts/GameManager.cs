32a18eca0f43b9391676f6512943ec61ea5d7d33
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-12-14 12:10:43 +1100

    Intro text implrementation

--- Assets/Team Members/John/Scripts/GameManager.cs
+++ Assets/Team Members/John/Scripts/GameManager.cs
@@ -8,10 +8,13 @@ public class GameManager : MonoBehaviour
     public NPCManager NpcManager;
     public SoundManager soundManager;
     public float startDelay;
+    public float introTextTime;
+    public GameObject introText;
     // Start is called before the first frame update
     void Start()
     {
         NpcManager = FindObjectOfType<NPCManager>();
+        StartCoroutine(IntroText());
         StartCoroutine(FirstWave());
     }
 
@@ -20,6 +23,12 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(startDelay);
         StartCoroutine(NpcManager.Spawn());
         soundManager.PlayOST();
+    }
+
+    public IEnumerator IntroText()
+    {
+        yield return new WaitForSeconds(introTextTime);
+        introText.SetActive(false);
     }
 
 }
